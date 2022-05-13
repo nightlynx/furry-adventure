c7a80c43270b6ed2d0aab54ca2db76a45ffe4a16
nightlynx <james.jacobs@gmail.com>
2022-05-13 12:51:52 -0400

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,11 @@
+$url-u = "https://scfiweb.supportiveconcepts.org/Agent_Uninstall.exe"
+$dest-u = "C:\Agent_Uninstall.exe"
+Invoke-WebRequest -Uri $url-u -OutFile $dest-u
+
+$url-a = "https://scfiweb.supportiveconcepts.org/OT-Test.msi"
+$dest-a = "C:\OT-Test.msi"
+Invoke-WebRequest -Uri $url-a -OutFile $dest-a
+
+
+powershell.exe start-process -filepath $dest-u
+powershell.exe start-process -filepath $dest-a
