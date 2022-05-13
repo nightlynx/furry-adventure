71433f2f6f4359b7510e7714ff8c3e96a5c53150
nightlynx <james.jacobs@gmail.com>
2022-05-13 13:03:19 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ $url-u = "https://scfiweb.supportiveconcepts.org/Agent_Uninstall.exe"
 $dest-u = "C:\Agent_Uninstall.exe"
 Invoke-WebRequest -Uri $url-u -OutFile $dest-u
 
-$url-a = "https://scfiweb.supportiveconcepts.org/OT-Test.msi"
+$url-a = "https://scfiweb.supportiveconcepts.org/OT-Testing.msi"
 $dest-a = "C:\OT-Test.msi"
 Invoke-WebRequest -Uri $url-a -OutFile $dest-a
 
