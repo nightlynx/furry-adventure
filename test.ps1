45f5c1c9ed82b355423fef13d1c111b325fbdfda
nightlynx <james.jacobs@gmail.com>
2022-05-13 13:20:42 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe -Outfile Agent_Uninstall.exe
-Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/OT-Testing.MSI -Outfile OT-Testing.MSI
+Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe -Outfile C:\Agent_Uninstall.exe
+Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/OT-Testing.MSI -Outfile C:\OT-Testing.MSI
 
 powershell.exe start-process -filepath C:\Agent_Uninstall.exe
 powershell.exe start-process -filepath C:\OT-Testing.MSI
