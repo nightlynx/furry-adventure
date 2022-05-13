466bc2bc15820192995bb976740874e5f1a6fb04
nightlynx <james.jacobs@gmail.com>
2022-05-13 13:16:09 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,11 +1,6 @@
-$url-u = "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe"
-$dest-u = "C:\Agent_Uninstall.exe"
-Invoke-WebRequest -Uri $url-u -OutFile $dest-u
-
-$url-a = "https://github.com/nightlynx/furry-adventure/raw/main/OT-Testing.MSI"
-$dest-a = "C:\OT-Test.msi"
-Invoke-WebRequest -Uri $url-a -OutFile $dest-a
-
+Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe -Outfile Agent_Uninstall.exe
+Invoke-WebRequest -Uri https://github.com/nightlynx/furry-adventure/raw/main/OT-Testing.MSI -Outfile OT-Testing.MSI
 
 powershell.exe start-process -filepath $dest-u
 powershell.exe start-process -filepath $dest-a
+wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
