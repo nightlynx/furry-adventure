88018fcf6096a078b7d03b33b9b8cd9c322dd580
nightlynx <james.jacobs@gmail.com>
2022-05-16 08:10:29 -0400

    Create Agent_Install_ALCO.ps1

---
+++ Agent_Install_ALCO.ps1
@@ -0,0 +1,10 @@
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe" -Outfile "C:\Agent_Uninstall.exe"
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Install_ALCO.MSI" -Outfile "C:\Agent_Install_ALCO.MSI"
+
+powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
+wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
+powershell.exe start-process -filepath "C:\Agent_Install_ALCO.MSI /qn" -Wait
+
+Remove-Item "C:\Agent_Uninstall.exe"
+Remove-Item "C:\Agent_Install_ALCO.MSI"
+Remove-Item "C:\Agent_Install_ALCO.ps1"
