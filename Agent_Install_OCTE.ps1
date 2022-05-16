d83dccc3c354f2925c920228611724aac4f8a1e6
nightlynx <james.jacobs@gmail.com>
2022-05-16 08:25:25 -0400

    Update and rename test.ps1 to Agent_Install_OCTE.ps1

--- Agent_Install_OCTE.ps1
+++ Agent_Install_OCTE.ps1
@@ -1,10 +1,10 @@
 Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe" -Outfile "C:\Agent_Uninstall.exe"
-Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/OT-Testing.MSI" -Outfile "C:\OT-Testing.MSI"
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Install_OCTE.MSI" -Outfile "C:\Agent_Install_OCTE.MSI"
 
 powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
 wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
-powershell.exe start-process -filepath "C:\OT-Testing.MSI /qn" -Wait
+powershell.exe start-process -filepath "C:\Agent_Install_OCTE.MSI /qn" -Wait
 
 Remove-Item "C:\Agent_Uninstall.exe"
-Remove-Item "C:\OT-Testing.MSI"
-Remove-Item "C:\test.ps1"
+Remove-Item "C:\Agent_Install_OCTE.MSI"
+Remove-Item "C:\Agent_Install_OCTE.ps1"
