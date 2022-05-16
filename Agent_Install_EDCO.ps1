6d75e0a8e9c5fecba9a7483451003a0b6cdc8a4f
nightlynx <james.jacobs@gmail.com>
2022-05-16 08:17:41 -0400

    Create Agent_Install_EDCO.ps1

---
+++ Agent_Install_EDCO.ps1
@@ -0,0 +1,10 @@
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe" -Outfile "C:\Agent_Uninstall.exe"
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Install_EDCO.MSI" -Outfile "C:\Agent_Install_EDCO.MSI"
+
+powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
+wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
+powershell.exe start-process -filepath "C:\Agent_Install_EDCO.MSI /qn" -Wait
+
+Remove-Item "C:\Agent_Uninstall.exe"
+Remove-Item "C:\Agent_Install_EDCO.MSI"
+Remove-Item "C:\Agent_Install_EDCO.ps1"
