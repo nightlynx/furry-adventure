eabd3db9b37c2739298c57822383a406c0460e0a
nightlynx <james.jacobs@gmail.com>
2022-05-16 08:20:22 -0400

    Create Agent_Install_PDCS.ps1

---
+++ Agent_Install_PDCS.ps1
@@ -0,0 +1,10 @@
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Uninstall.exe" -Outfile "C:\Agent_Uninstall.exe"
+Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Agent_Install_PDCS.MSI" -Outfile "C:\Agent_Install_PDCS.MSI"
+
+powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
+wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
+powershell.exe start-process -filepath "C:\Agent_Install_PDCS.MSI /qn" -Wait
+
+Remove-Item "C:\Agent_Uninstall.exe"
+Remove-Item "C:\Agent_Install_PDCS.MSI"
+Remove-Item "C:\Agent_Install_PDCS.ps1"
