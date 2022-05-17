4053846fd59f3a112222398f5f101abf17a1e2f4
nightlynx <james.jacobs@gmail.com>
2022-05-17 09:14:33 -0400

    Update Agent_Install_OCTE.ps1

--- Agent_Install_OCTE.ps1
+++ Agent_Install_OCTE.ps1
@@ -3,7 +3,9 @@ Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Ag
 
 powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
 wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
+Start-Sleep -Seconds 60
 powershell.exe start-process -filepath "C:\Agent_Install_OCTE.MSI /qn" -Wait
 
 Remove-Item "C:\Agent_Uninstall.exe"
 Remove-Item "C:\Agent_Install_OCTE.MSI"
+Remove-Item "C:\Agent_Install_OCTE.ps1"
