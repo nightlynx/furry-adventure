15b54e2d348e1cebf4c343375e256b4d188ef039
nightlynx <james.jacobs@gmail.com>
2022-05-17 09:20:13 -0400

    Update Agent_Install_ALCO.ps1

--- Agent_Install_ALCO.ps1
+++ Agent_Install_ALCO.ps1
@@ -3,6 +3,7 @@ Invoke-WebRequest -Uri "https://github.com/nightlynx/furry-adventure/raw/main/Ag
 
 powershell.exe start-process -filepath "C:\Agent_Uninstall.exe" -Wait
 wmic product where "Name like '%%ScreenConnect%%'" call uninstall /nointeractive
+Start-Sleep -Seconds 60
 powershell.exe start-process -filepath "C:\Agent_Install_ALCO.MSI /qn" -Wait
 
 Remove-Item "C:\Agent_Uninstall.exe"
